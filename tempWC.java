be5bc6808ef0e1e14f2f6afcdd68b9cc41995593
Emma Trzupek <supertrzup17@gmail.com>
2024-10-31 21:38:45 -0400

    10/14

--- tempWC.java
+++ tempWC.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.io.*;
 public class WordCounter {
 	public static int processText(StringBuffer text,String stopword) throws TooSmallText, InvalidStopwordException{
-		System.out.println("In processText");
 		Pattern regex = Pattern.compile("\\b[a-zA-Z0-9']+");
 		Matcher regexMatcher = regex.matcher(text);
 		int count=1;
@@ -11,7 +10,6 @@ public class WordCounter {
 		    	String word=regexMatcher.group();
 			if(word.equals(stopword)){
 				if(count<5){
-					System.out.println("Throwing TooSmallText");
 					throw new TooSmallText("Only found "+(count+1)+" words.");
 				}
 				return(count);
@@ -19,17 +17,14 @@ public class WordCounter {
 			count++;
 		}
 		if(count<6){
-			System.out.println("Throwing TooSmallText");
 			throw new TooSmallText("Only found "+(count-1)+" words.");
 		}
 		if(stopword==null){
 			return(count-1);
 		} 
-		System.out.println("Throwing stopWord not found");
 		throw new InvalidStopwordException("Couldn't find stopword: "+stopword);
 	}
 	public static StringBuffer processFile(String path) throws EmptyFileException{ 
-		System.out.println("in processFile ");
 		String cPath=path;
 		Scanner kb=new Scanner(System.in);
 		Scanner s;
@@ -43,7 +38,7 @@ public class WordCounter {
 			}
 			break;
 		}if(!s.hasNext()){
-			System.out.println("throwing empty exception\n");
+			System.out.println("Empty exception\n");
 			throw new EmptyFileException(path+" was empty");
 		}
 		String str="";
@@ -54,42 +49,22 @@ public class WordCounter {
 			}
 		}
 		StringBuffer buf=new StringBuffer(str);
-		System.out.println("Exiting processFile");
 		return(buf);
 	}
-	public static void main(String[] args) throws Exception{
-		System.out.println("in main\n");
+	public static void main(String[] args){
 		Scanner kb=new Scanner(System.in);
 		int fileProgress=0;
 		int choice;
-		int count=0;
-		System.out.println(args.length);
-		for(int i=0;i<args.length;i++){System.out.println(args[i]);}
 		for(choice=-1;choice!=1&&choice!=2;choice=kb.nextInt()){
-			count++;
-			if(count>100){throw(new Exception("Infinite Loop"));}
-			System.out.println("main menu looped");
 			System.out.println("Would you like to (1) process a file or (2) process text?");
 		}
 		if(choice==1){		
 			while(true){
-			count++;
-				if(count>100){throw(new Exception("Infinite Loop"));}
-				System.out.println("Choice 1 looped");
 				try{
-					count++;
-					System.out.println("Input the path "+fileProgress+" "+args.length);
-					String path;
-					if(args.length>fileProgress){
-						System.out.println("path is an argument");
-						path=args[fileProgress++];
-					}else{
-						System.out.println("path is a keyboard input");
-						path=kb.next();
-					}
-					
+					System.out.println("Input the path");
+					String path=args.length<fileProgress?args[fileProgress++]:kb.next();
 					System.out.println("Input the stop Word");
-					String stopWord=(args.length>fileProgress)?args[fileProgress++]:kb.next();
+					String stopWord=args.length<fileProgress?args[fileProgress++]:kb.next();
 					System.out.println(processText(processFile(path),stopWord));
 				}catch(Exception e){
 					System.out.println("Exception found");
@@ -100,14 +75,11 @@ public class WordCounter {
 		}
 		if(choice==2){		
 			while(true){
-				count++;
-				if(count>100){throw(new Exception("Infinite Loop"));}
 				try{
-				System.out.println("choice 2 looped");
 					System.out.println("Input the text");
-					String text=(args.length>fileProgress)?args[fileProgress++]:kb.nextLine();
+					String text=args.length<fileProgress?args[fileProgress++]:kb.nextLine();
 					System.out.println("Input the stop Word");
-					String stopWord=(args.length>fileProgress)?args[fileProgress++]:kb.next();
+					String stopWord=args.length<fileProgress?args[fileProgress++]:kb.next();
 					System.out.println(processText(new StringBuffer(text),stopWord));
 				}catch(Exception e){
 					System.out.println("Found exception");
